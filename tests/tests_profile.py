7669f0c472ced7dc5dcf028b47ce23e5f8694834
Dave Kludt <oldarmyc@gmail.com>
2019-01-03 12:51:30 -0600

    Add a few tests

--- tests/tests_profile.py
+++ tests/tests_profile.py
@@ -1,4 +1,8 @@
 
+from system_profile import profile
+
+
+import system_profile
 import logging
 import sys
 
@@ -9,13 +13,13 @@ else:
     from unittest2 import TestCase
 
 
-# try:
-#     from unittest import mock
-# except ImportError:
-#     import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 
-class TestProfile(TestCase):
+class TestSystemProfile(TestCase):
     def setUp(self):
         logging.disable(logging.ERROR)
 
@@ -23,5 +27,50 @@ class TestProfile(TestCase):
         logging.disable(logging.NOTSET)
 
     def test_version(self):
-        # Sample test to ensure things all run
-        assert True
+        self.assertEquals(
+            system_profile.__version__,
+            '0.1.0',
+            'Version does not match expected value'
+        )
+
+    def test_execute_command_success(self):
+        test_command = ['getenforce']
+        selinux_status = None
+        with mock.patch('system_profile.profile.Popen') as popen:
+            popen.return_value.communicate.return_value = ('enforcing', '')
+            popen.return_value.returncode = 0
+            selinux_status = profile.execute_command(test_command, False)
+
+        self.assertEqual(
+            selinux_status,
+            'enforcing',
+            'Status does not equal expected output'
+        )
+
+    def test_execute_command_error(self):
+        test_command = ['getenforce']
+        selinux_status = None
+        with mock.patch('system_profile.profile.Popen') as popen:
+            popen.return_value.communicate.return_value = ('', 'Error')
+            popen.return_value.returncode = 1
+            selinux_status = profile.execute_command(test_command, False)
+
+        self.assertEqual(
+            selinux_status,
+            '',
+            'Status does not equal expected output'
+        )
+
+    def test_execute_command_verbose_success(self):
+        test_command = ['getenforce']
+        selinux_status = None
+        with mock.patch('system_profile.profile.Popen') as popen:
+            popen.return_value.communicate.return_value = ('enforcing', '')
+            popen.return_value.returncode = 0
+            selinux_status = profile.execute_command(test_command, True)
+
+        self.assertEqual(
+            selinux_status,
+            'enforcing',
+            'Status does not equal expected output'
+        )
