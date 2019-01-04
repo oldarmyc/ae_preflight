7577977cee0160fc4adaf6d1da2768f3647fbf8b
Dave Kludt <oldarmyc@gmail.com>
2019-01-04 13:43:16 -0600

    Add import statement

--- tests/tests_reporting.py
+++ tests/tests_reporting.py
@@ -1,4 +1,5 @@
 
+from __future__ import absolute_import
 from .fixtures import reporting_returns
 from system_profile import profile
 
--- tests/tests_system_gather.py
+++ tests/tests_system_gather.py
@@ -1,4 +1,5 @@
 
+from __future__ import absolute_import
 from .fixtures import command_returns
 from system_profile import profile
 
